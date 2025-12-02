6291a94d9840faada6dc507f6c0485a42a08f886
prettysunflower <me@prettysunflower.moe>
2025-12-02 10:41:23 -0500

    feat: Added tests for personal puzzle input
    Signed-off-by: prettysunflower <me@prettysunflower.moe>

--- day1/tests/part1.rs
+++ day1/tests/part1.rs
@@ -1,10 +1,16 @@
 mod inputs;
 
 use day1::puzzle_part1;
-use crate::inputs::PUZZLE_INPUT_1;
+use crate::inputs::{PUZZLE_INPUT_1, PUZZLE_INPUT_2};
 
 #[test]
 fn test_puzzle_part1() {
     let result = puzzle_part1(&PUZZLE_INPUT_1.to_vec());
     assert_eq!(result, 3);
+}
+
+#[test]
+fn test_puzzle_part1_self_puzzle_input() {
+    let result = puzzle_part1(&PUZZLE_INPUT_2.to_vec());
+    assert_eq!(result, 999);
 }
--- day1/tests/part2.rs
+++ day1/tests/part2.rs
@@ -1,7 +1,7 @@
 mod inputs;
 
 use day1::puzzle_part2;
-use crate::inputs::PUZZLE_INPUT_1;
+use crate::inputs::{PUZZLE_INPUT_1, PUZZLE_INPUT_2};
 
 #[test]
 fn test_puzzle_part2() {
@@ -15,4 +15,10 @@ fn test_puzzle_part2_careful_warning() {
         "R1000"
     ]);
     assert_eq!(result, 10);
+}
+
+#[test]
+fn test_puzzle_part2_self_puzzle_input() {
+    let result = puzzle_part2(&PUZZLE_INPUT_2.to_vec());
+    assert_eq!(result, 6099);
 }
