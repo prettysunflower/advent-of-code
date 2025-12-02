fee0106d8c53327bcd32527ff4499e81665ede53
prettysunflower <me@prettysunflower.moe>
2025-12-02 11:17:59 -0500

    refactor(Rust 2025-02): Extracted the shared get_bounds method
    Signed-off-by: prettysunflower <me@prettysunflower.moe>

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -1,14 +1,18 @@
-pub fn puzzle_part1(input: &str) -> u64 {
+fn get_bounds(range: &str) -> (usize, usize) {
+    let (lower_bound_str, upper_bound_str) = range.split_once("-").expect("invalid range string");
+    (
+        lower_bound_str.parse::<usize>().unwrap_or_else(|_| panic!("{} is not a valid number", lower_bound_str)),
+        upper_bound_str.parse::<usize>().unwrap_or_else(|_| panic!("{} is not a valid number", lower_bound_str))
+    )
+}
+
+pub fn puzzle_part1(input: &str) -> usize {
     let ranges: Vec<&str> = input.split(",").collect();
 
-    let mut sum: u64 = 0;
+    let mut sum: usize = 0;
 
     for range in ranges {
-        let (lower_bound_str, upper_bound_str) = range.split_once("-").expect("invalid range string");
-        let (lower_bound, upper_bound) = (
-            lower_bound_str.parse::<u64>().unwrap_or_else(|_| panic!("{} is not a valid number", lower_bound_str)),
-            upper_bound_str.parse::<u64>().unwrap_or_else(|_| panic!("{} is not a valid number", lower_bound_str))
-        );
+        let (lower_bound, upper_bound) = get_bounds(range);
 
         for number in lower_bound..=upper_bound {
             let str_num = number.to_string();
@@ -22,17 +26,13 @@ pub fn puzzle_part1(input: &str) -> u64 {
     sum
 }
 
-pub fn puzzle_part2(input: &str) -> u64 {
+pub fn puzzle_part2(input: &str) -> usize {
     let ranges: Vec<&str> = input.split(",").collect();
 
-    let mut sum: u64 = 0;
+    let mut sum: usize = 0;
 
     for range in ranges {
-        let (lower_bound_str, upper_bound_str) = range.split_once("-").expect("invalid range string");
-        let (lower_bound, upper_bound) = (
-            lower_bound_str.parse::<u64>().unwrap_or_else(|_| panic!("{} is not a valid number", lower_bound_str)),
-            upper_bound_str.parse::<u64>().unwrap_or_else(|_| panic!("{} is not a valid number", lower_bound_str))
-        );
+        let (lower_bound, upper_bound) = get_bounds(range);
 
         'number_loop: for number in lower_bound..=upper_bound {
             let str_num = number.to_string();
